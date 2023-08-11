6f7882aafb0ae597bb76af6293390248b9c22280
Olesya090585 <okochman@ya.ru>
2023-08-11 11:15:21 +0300

    Project15

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -111,7 +111,7 @@ export class Api {
   }
 }
 const api = new Api({
-  baseUrl: "https://api.mesto.ovarnakova.nomoreparties.co/",
+  baseUrl: "https://api.mesto.ovarnakova.nomoreparties.co",
   headers: {
     "Content-Type": "application/json",
   },
